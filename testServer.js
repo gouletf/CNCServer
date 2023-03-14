e609b0193376214820ba54df3056b40b970a396b
gouletf <77026560+gouletf@users.noreply.github.com>
2023-03-14 19:46:37 -0400

    Sensor Update

--- testServer.js
+++ testServer.js
@@ -2,17 +2,24 @@ let express = require('express');
 let cors = require('cors')
 let config = require('./config');
 let fs = require('fs');
+let sensor = require('ds18x20');
 let server = express();
 
 server.use(cors());
 
 const PORT = config.app.PORT || 8080;
 const intervals = config.cnc.chartIntervals || 30;
-const fetchRate = config.cnc.fetchRate || 5000;
 
 let temperature;
 let temperatures = [];
 let sensorTemperatures = [];
+
+sensor.loadDriver(function (err) {
+   if (err) console.log('something went wrong loading the driver:', err)
+   else console.log('driver is loaded');
+});
+
+let listOfSensorIds = sensor.list();
 
 server.get('/temperature', (req, res) => {
    getTemperature();
@@ -25,18 +32,23 @@ server.get('/config', (req, res) => {
 
 server.listen(PORT, () => {
    console.log(`server running on port ${PORT}`);
-   config.sensors.forEach(s => {
-	  sensorTemperatures.push({name: s.name, data: []})
+   listOfSensorIds.forEach(s => {
+     let name = s.id;
+     let override = config.sensors.filter(c => c.id === s);
+     if(override !== undefined && override.name !== undefined && override.name !== null) {
+      name = override.name;
+     }
+	  sensorTemperatures.push({name: s.name, id:s.id, data: []})
    })
 });
 
 function getTemperature() {
   sensorTemperatures.forEach(s => {
-	  temperature = Math.floor(Math.random() * 100);
+	  
 	  s.data.forEach(t => {
 		  t.id = t.id -1;
 	  });
-	   s.data.push({id: intervals, temp: temperature})
+	   s.data.push({id: intervals, temp: sensor.get(s.id)})
 	   if(s.data.length >= intervals + 1) {
 			s.data.shift();
 		}
